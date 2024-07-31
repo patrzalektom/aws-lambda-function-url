f0fedfc47f4e4af184942f5fd4674ddfe1652121
patrzalektom <patrzalek.tom@gmail.com>
2024-08-01 00:04:24 +0200

    Initial commit

---
+++ lambda-function-url-v1/src/main/java/tom/patrzalek/lambdafunctionurlv1/LambdaFunctionUrlV1Application.java
@@ -0,0 +1,13 @@
+package tom.patrzalek.lambdafunctionurlv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaFunctionUrlV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaFunctionUrlV1Application.class, args);
+    }
+
+}
---
+++ lambda-function-url-v2/src/test/java/tom/patrzalek/lambdafunctionurlv2/LambdaFunctionUrlV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tom.patrzalek.lambdafunctionurlv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaFunctionUrlV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
